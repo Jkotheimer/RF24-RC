b762ff91c67ec798b0e20eff1d2be7c3225a52e7
Jack <jkotheimer9@gmail.com>
2025-09-05 23:11:41 -0500

    Added steering motor controls

--- RFReceiver/RFReceiver.ino
+++ RFReceiver/RFReceiver.ino
@@ -9,7 +9,9 @@
 
 #define DRIVE_MOTOR_PIN_1 4
 #define DRIVE_MOTOR_PIN_2 5
-#define HBRIDGE_INTERRUPT_PIN 2
+#define STEERING_MOTOR_PIN_1 6
+#define STEERING_MOTOR_PIN_2 7
+#define H_BRIDGE_INTERRUPT_PIN 2
 
 #define JOYSTICK_LOWER_BOUND -128
 #define JOYSTICK_UPPER_BOUND 127
@@ -28,6 +30,11 @@
 const uint8_t CONTROLLER_ADDRESS[5] = { 0x45, 0x12, 0x78, 0x08, 0xAD };
 const uint8_t RECEIVER_ADDRESS[5] = { 0x46, 0x12, 0x78, 0x08, 0xAD };
 
+/**
+ * ----------------------------------------------
+ * --------------- STRUCT DEFS ------------------
+ * ----------------------------------------------
+ */
 struct ControlPacket {
   char id = CONTROL_PACKET_ID;
   int8_t vertical = 0;    // Vertical (forward / back) velocity
@@ -36,6 +43,10 @@ struct ControlPacket {
 
   ControlPacket() {}
   ControlPacket(void *data) {
+    apply(data);
+  }
+
+  void apply(void *data) {
     char *p0 = (char *)data;
     id = *p0;
     int8_t *p1 = (int8_t *)(p0 + 1);
@@ -65,8 +76,6 @@ struct ControlPacket {
     Serial.println(select ? "True" : "False");
   }
 };
-ControlPacket *previousControllerState;
-ControlPacket *currentControllerState;
 
 struct AckPacket {
   char id = ACK_PACKET_ID;
@@ -74,6 +83,9 @@ struct AckPacket {
 
   AckPacket() {}
   AckPacket(void *data) {
+    apply(data);
+  }
+  void apply(void *data) {
     char *p0 = (char *)data;
     id = *p0;
     uint8_t *p1 = (uint8_t *)(data + 1);
@@ -106,7 +118,7 @@ struct PWM {
 
   void activate() {
     isActive = true;
-    ping();
+    pulse();
   }
 
   void deactivate() {
@@ -121,7 +133,7 @@ struct PWM {
   void setState(bool state) {
     if (isHigh != state) {
       isHigh = state;
-      digitalWrite(pin, isHigh ? HIGH : LOW);
+      digitalWrite(pin, isHigh);
       if (isHigh) {
         previousHighTick = micros();
       }
@@ -147,7 +159,7 @@ struct PWM {
     Serial.println(dutyCycle, 4);
   }
 
-  void ping() {
+  void pulse() {
     if (!isActive || dutyCycle < PWM_MIN_DUTY_CYCLE) {
       setState(false);
       return;
@@ -155,94 +167,148 @@ struct PWM {
     
     // Wait until it is time to drop to low state
     uint32_t currentPulseLength = (uint32_t)((float)pulseDivisionMicros * (isHigh ? dutyCycle : (1.0 - dutyCycle)));
-    uint32_t timeToSleep = micros() - previousHighTick + currentPulseLength;
-    delayMicroseconds(timeToSleep);
+    int32_t timeToSleep = micros() - previousHighTick + currentPulseLength;
+    if (timeToSleep > 0) {
+      delayMicroseconds(timeToSleep);
+    }
     setState(!isHigh);
   }
 };
-PWM *driveMotorPWM1;
-PWM *driveMotorPWM2;
+
+struct Motor {
+  PWM *pwm1;
+  PWM *pwm2;
+  bool trackPosition = false;
+  int8_t velocity = 0;
+  int8_t position = 0;
+  int8_t nextPosition = 0;
+
+  Motor(uint8_t pin1, uint8_t pin2) {
+    init(pin1, pin2);
+  }
+
+  Motor(uint8_t pin1, uint8_t pin2, bool tp) {
+    init(pin1, pin2);
+    trackPosition = tp;
+  }
+
+  void init(uint8_t pin1, uint8_t pin2) {
+    pwm1 = new PWM(pin1);
+    pwm2 = new PWM(pin2);
+  }
+
+  void setPosition(int8_t p) {
+    nextPosition = p;
+  }
+
+  void setVelocity(int8_t v) {
+    velocity = v;
+  }
+
+  void stop() {
+    pwm1->deactivate();
+    pwm2->deactivate();
+  }
+
+  void pulse() {
+    //currentControllerState->print();
+    if (position != nextPosition) {
+      velocity = nextPosition - position;
+    }
+    if (velocity > 0) {
+      // Forward drive
+      pwm1->deactivate();
+      pwm2->setDutyCycleFromAnalogValue(velocity);
+      pwm2->activate();
+      if (trackPosition) {
+        position++;
+      }
+    } else if (velocity < 0) {
+      // Reverse drive
+      pwm2->deactivate();
+      pwm1->setDutyCycleFromAnalogValue(abs(velocity));
+      pwm1->activate();
+      if (trackPosition) {
+        position--;
+      }
+    } else {
+      // Stop
+      stop();
+    }
+  }
+};
 
 // TODO : Implement channel change request handling
 uint8_t radioChannel = 69;
 
-RF24 radio(CE_PIN, CSN_PIN, 4000000);
+// Global static variales
+RF24 *radio = new RF24(CE_PIN, CSN_PIN, 4000000);
+ControlPacket *currentControllerState = new ControlPacket();
+Motor *driveMotor = new Motor(DRIVE_MOTOR_PIN_1, DRIVE_MOTOR_PIN_2);
+Motor *steeringMotor = new Motor(STEERING_MOTOR_PIN_1, STEERING_MOTOR_PIN_2, true);
 
+/**
+ * ----------------------------------------------
+ * -------------- SETUP & LOOP ------------------
+ * ----------------------------------------------
+ */
 void setup() {
   while (!Serial) {}
   Serial.begin(SERIAL_BAUD_RATE);
   while (!Serial) {}
   printf_begin();
 
-  driveMotorPWM1 = new PWM(DRIVE_MOTOR_PIN_1);
-  driveMotorPWM2 = new PWM(DRIVE_MOTOR_PIN_2);
-  previousControllerState = new ControlPacket();
-  currentControllerState = new ControlPacket();
-
-  attachInterrupt(digitalPinToInterrupt(HBRIDGE_INTERRUPT_PIN), handleHBridgeFailure, FALLING);
+  attachInterrupt(digitalPinToInterrupt(H_BRIDGE_INTERRUPT_PIN), handleHBridgeFailure, FALLING);
 
   resetRadio();
-  radio.printPrettyDetails();
+  radio->printPrettyDetails();
 }
 
 void loop() {
-  operateDriveMotor();
-  checkRadioSignal();
-}
-
-void operateDriveMotor() {
-  //currentControllerState->print();
-  if (currentControllerState->vertical > 0) {
-    // Forward drive
-    driveMotorPWM1->deactivate();
-    driveMotorPWM2->setDutyCycleFromAnalogValue(currentControllerState->vertical);
-    driveMotorPWM2->activate();
-  } else if (currentControllerState->vertical < 0) {
-    // Reverse drive
-    driveMotorPWM2->deactivate();
-    driveMotorPWM1->setDutyCycleFromAnalogValue(abs(currentControllerState->vertical));
-    driveMotorPWM1->activate();
-  } else {
-    // Stop
-    driveMotorPWM1->deactivate();
-    driveMotorPWM2->deactivate();
+  ControlPacket *controllerState = readControllerState();
+  if (controllerState != NULL) {
+    controllerState->print();
+    driveMotor->setVelocity(controllerState->vertical);
+    steeringMotor->setPosition(controllerState->horizontal);
+    delete controllerState;
   }
+  driveMotor->pulse();
+  steeringMotor->pulse();
 }
 
 void handleHBridgeFailure() {
   // Stop the motors on failure
   // This usually happens when you change directions too fast
-  driveMotorPWM1->deactivate();
-  driveMotorPWM2->deactivate();
+  driveMotor->stop();
+  steeringMotor->stop();
 }
 
 uint8_t unavailableCount = 0;
-void checkRadioSignal() {
-  if (!radio.available()) {
+ControlPacket *readControllerState() {
+  if (!radio->available()) {
     if (++unavailableCount == 0) {
       resetRadio();
     } else {
       delayMicroseconds(100);
     }
-    return;
+    return NULL;
   }
   unavailableCount = 0;
 
-  uint8_t payloadSize = radio.getDynamicPayloadSize();  // get the size of the payload
+  uint8_t payloadSize = radio->getDynamicPayloadSize();  // get the size of the payload
   if (payloadSize == 0 || payloadSize > MAX_ALLOWED_PAYLOAD_SIZE) {
-    radio.flush_rx();
-    return;
+    radio->flush_rx();
+    return NULL;
   }
+  ControlPacket *packet;
   void *data = (void *)malloc(payloadSize);
   realloc(data, payloadSize);
-  radio.read(data, payloadSize);
+  radio->read(data, payloadSize);
   char id = *((char *)data);
-  if (id == ACK_PACKET_ID) {
+  if (ACK_PACKET_ID == id) {
     handleAckRequest(data);
-  } else if (id == CONTROL_PACKET_ID) {
-    delete previousControllerState;
-    previousControllerState = currentControllerState;
-    currentControllerState = new ControlPacket(data);
+  } else if (CONTROL_PACKET_ID == id) {
+    packet = new ControlPacket(data);
   } else {
     Serial.print(F("ERROR: Unexpected payload: "));
     uint8_t *di = (uint8_t *)data;
@@ -251,68 +317,56 @@ void checkRadioSignal() {
       Serial.print(F(" "));
     }
     Serial.println(F(""));
-    delete previousControllerState;
-    previousControllerState = currentControllerState;
-    currentControllerState = new ControlPacket();
   }
   free(data);
+  return packet;
 }
 
 void handleAckRequest(void *data) {
   AckPacket *packet = new AckPacket(data);
-  /*
-  Serial.print("Received ACK Request for ");
-  Serial.print(packet->listenTimeoutMillis);
-  Serial.print("ms. ");
-  */
   const unsigned long startTime = millis();
   char *serializedResponsePacket = packet->serialize();
   while (millis() - startTime < packet->listenTimeoutMillis) {
-    operateDriveMotor();
 
-    radio.stopListening();
+    driveMotor->pulse();
+    steeringMotor->pulse();
+
+    radio->stopListening();
     delayMicroseconds(150);
-    radio.writeFast(serializedResponsePacket, sizeof(AckPacket));
-    radio.txStandBy(0, true);
-    radio.startListening();
-
-    operateDriveMotor();
-    /*
-    Serial.print(" ACK Written ");
-    Serial.print(sizeof(AckPacket));
-    */
+    radio->writeFast(serializedResponsePacket, sizeof(AckPacket));
+    radio->txStandBy(0, true);
+    radio->startListening();
 
     // If a packet is available, stop sending ACKs and handle the packet
-    if (radio.available() && radio.getDynamicPayloadSize() > 0) {
+    if (radio->available() && radio->getDynamicPayloadSize() > 0) {
       break;
     }
   }
   free(serializedResponsePacket);
-  delete (packet);
-  //Serial.println(F(""));
+  delete packet;
 }
 
 void resetRadio() {
   // initialize the transceiver on the SPI bus
-  while (!radio.begin()) {
+  while (!radio->begin()) {
     Serial.println(F("radio hardware is not responding!"));
     delay(10);
   }
-  radio.failureDetected = 0;
-  radio.txDelay = 0;
-  radio.csDelay = 0;
+  radio->failureDetected = 0;
+  radio->txDelay = 0;
+  radio->csDelay = 0;
 
-  radio.setPALevel(RF24_PA_LOW);
-  radio.setCRCLength(RF24_CRC_16);
-  radio.setChannel(radioChannel);
-  radio.setAutoAck(false);
+  radio->setPALevel(RF24_PA_LOW);
+  radio->setCRCLength(RF24_CRC_16);
+  radio->setChannel(radioChannel);
+  radio->setAutoAck(false);
 
-  radio.enableDynamicPayloads();
-  radio.enableDynamicAck();
+  radio->enableDynamicPayloads();
+  radio->enableDynamicAck();
 
-  radio.stopListening(CONTROLLER_ADDRESS);
-  radio.openReadingPipe(1, RECEIVER_ADDRESS);
-  radio.startListening();
+  radio->stopListening(CONTROLLER_ADDRESS);
+  radio->openReadingPipe(1, RECEIVER_ADDRESS);
+  radio->startListening();
 
   delayMicroseconds(150);
 }
